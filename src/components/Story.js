f0037794082ddb385df01f011378f83fa8fd7324
curtyraissa <raissacurty@gmail.com>
2023-02-11 16:02:25 -0300

    criado objStory e implementado metodo map

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,79 +1,30 @@
 import { Usuario } from "./Usuario"
 
 export const Story = () => {
-    return (
-        <>
-            <div className="story">
-                <div className="imagem">
-                    <img src="assets/9gag.svg" alt="9gag" />
-                </div>
-                <Usuario>
-                    9gag
-                </Usuario>
-            </div>
+  const objStory = [
+    { src: "assets/9gag.svg", alt: "9gag" },
+    { src: "assets/meowed.svg", alt: "meowed" },
+    { src: "assets/barked.svg", alt: "barked" },
+    { src: "assets/nathanwpylestrangeplanet.svg", alt: "nathanwpylestrangeplanet" },
+    { src: "assets/wawawicomics.svg", alt: "wawawicomics" },
+    { src: "assets/respondeai.svg", alt: "respondeai" },
+    { src: "assets/filomoderna.svg", alt: "filomoderna" },
+    { src: "assets/memeriagourmet.svg", alt: "memeriagourmet" }
+  ]
 
-            <div className="story">
-                <div className="imagem">
-                    <img src="assets/meowed.svg" alt="meowed" />
-                </div>
-                <Usuario>
-                    meowed
-                </Usuario>
-            </div>
+  return (
+    <>
+      {objStory.map(i =>
+        <div className="story">
+          <div className="imagem">
+            <img src="assets/9gag.svg" alt="9gag" />
+          </div>
+          <Usuario>
+            9gag
+          </Usuario>
+        </div>
+      )}
 
-            <div className="story">
-                <div className="imagem">
-                    <img src="assets/barked.svg" alt="barked" />
-                </div>
-                <Usuario>
-                    barked
-                </Usuario>
-            </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="assets/nathanwpylestrangeplanet.svg" alt="nathanwpylestrangeplanet" />
-                </div>
-                <Usuario>
-                    nathanwpylestrangeplanet
-                </Usuario>
-            </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="assets/wawawicomics.svg" alt="wawawicomics" />
-                </div>
-                <Usuario>
-                    wawawicomics
-                </Usuario>
-            </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="assets/respondeai.svg" alt="respondeai" />
-                </div>
-                <Usuario>
-                    respondeai
-                </Usuario>
-            </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="assets/filomoderna.svg" alt="filomoderna" />
-                </div>
-                <Usuario>
-                    filomoderna
-                </Usuario>
-            </div>
-
-            <div className="story">
-                <div className="imagem">
-                    <img src="assets/memeriagourmet.svg" alt="memeriagourmet" />
-                </div>
-                <Usuario>
-                    memeriagourmet
-                </Usuario>
-            </div>
-        </>
-    )
+    </>
+  )
 }
