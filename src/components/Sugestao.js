2ef7f3e5e5139d119533c725e4c53a78f745412a
curtyraissa <raissacurty@gmail.com>
2023-02-11 14:20:33 -0300

    criado array de objeto, e utilizando metodo map no componente sugestao

--- src/components/Sugestao.js
+++ src/components/Sugestao.js
@@ -1,27 +1,27 @@
-/*
 export const Sugestao = () => {
   const objSugestao = [
-    { src: "../../assets/bad.vibes.memes.svg", nome: "bad vibes memes", razao: "Segue você", seguir: "Seguir" },
-    { src: "../../assets/chibirdart.svg", nome: "chibirdart", razao: "Segue você", seguir: "Seguir" },
-    { src: "../../assets/razoesparaacreditar.svg", nome: "razoesparaacreditar", razao: "Novo no Instagram", seguir:"Seguir" },
-    { src: "../../assets/adorable_animals.svg", nome: "adorable_animals", razao: "Segue você", seguir: "Seguir" },
-    { src: "../../assets/smallcutecats.svg", nome: "smallcutecats", razao: "Segue você", seguir: "Seguir" }
+    { src: "assets/bad.vibes.memes.svg", nome: "bad vibes memes", razao: "Segue você", seguir: "Seguir" },
+    { src: "assets/chibirdart.svg", nome: "chibirdart", razao: "Segue você", seguir: "Seguir" },
+    { src: "assets/razoesparaacreditar.svg", nome: "razoesparaacreditar", razao: "Novo no Instagram", seguir: "Seguir" },
+    { src: "assets/adorable_animals.svg", nome: "adorable_animals", razao: "Segue você", seguir: "Seguir" },
+    { src: "assets/smallcutecats.svg", nome: "smallcutecats", razao: "Segue você", seguir: "Seguir" }
   ];
 
   return (
-    {objSugestao.map(sugestao => {
-      <div className="sugestao">
-      <div className="usuario">
-        <img src={sugestao.src} alt={sugestao.nome} />
-        <div className="texto">
-          <div className="nome">{sugestao.nome}</div>
-          <div className="razao">{sugestao.razao}</div>
+    <>
+      {objSugestao.map(i =>
+        <div className="sugestao">
+          <div className="usuario">
+            <img src={i.src} alt={i.nome} />
+            <div className="texto">
+              <div className="nome">{i.nome}</div>
+              <div className="razao">{i.razao}</div>
+            </div>
+          </div>
+          <div class="seguir">{i.seguir}</div>
         </div>
-      </div>
-      <div class="seguir">{sugestao.seguir}</div>
-    </div>}
       )}
-   
+    </>
+
   )
 }
-*/
