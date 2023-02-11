b08ff8e5a3f6c3a62cb08c2ae6a139d1c1cdbede
curtyraissa <raissacurty@gmail.com>
2023-02-11 14:58:24 -0300

    criado componente link, copyringt. criado arrayObj do sidebar e implementado metodo map

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,31 +2,50 @@ import { Sugestoes } from "./Sugestoes"
 import { Usuario } from "./Usuario"
 
 export const SideBar = () => {
-  const userName = "catanacomics"
+  const objSideBar = [
+    { src: "assets/img/catanacomics.svg", alt: "imagem de perfil", perfil: "catanacomics" }
+  ]
   const links = "Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma"
   const copyright = "© 2021 INSTAGRAM DO FACEBOOK"
 
   return (
     <div className="sidebar">
-      <Usuario>
-      <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
-        <div className="texto">
-          <span>
-            <strong>{userName}</strong>
-            <ion-icon name="pencil"></ion-icon>
-          </span>
-        </div>
-      </Usuario>
-
+      {objSideBar.map(i =>
+        <Usuario>
+          <img src={i.src} alt={i.alt} />
+          <div className="texto">
+            <span>
+              <strong>{i.perfil}</strong>
+              <ion-icon name="pencil"></ion-icon>
+            </span>
+          </div>
+        </Usuario>
+      )}
       <Sugestoes />
 
-      <div className="links">
+      <Links>
         {links}
-      </div>
+      </Links>
 
-      <div className="copyright">
-       {copyright}
-      </div>
+      <Copyright>
+        {copyright}
+      </Copyright>
+    </div>
+  )
+}
+
+const Links = (props) => {
+  return (
+    <div className="links">
+      {props.children}
+    </div>
+  )
+}
+
+const Copyright = (props) => {
+  return (
+    <div className="copyright">
+      {props.copyright}
     </div>
   )
 }
