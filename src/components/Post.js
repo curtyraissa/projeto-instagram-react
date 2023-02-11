61fb1c7bed13abd108856f76ff228037a28ed93b
curtyraissa <raissacurty@gmail.com>
2023-02-11 14:48:32 -0300

    criado array de objeto Post e implementado map, ternário

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,79 +1,52 @@
 import { Usuario } from "./Usuario"
 
 export const Post = () => {
+  const objPost = [
+    { srcTopo: "assets/meowed.svg", altTopo: "meowed", srcPost: "assets/gato-telefone.svg", altPost: "gato-telefone", srcLike: "assets/respondeai.svg", altLike: "respondeai", like: 101.523 },
+    { srcTopo: "assets/barked.svg", altTopo: "barked", srcPost: "assets/dog.svg", altPost: "dog", srcLike: "assets/adorable_animals.svg", altLike: "adorable_animals", like: 99.159 },
+    { srcTopo: "assets/meowed.svg", altTopo: "meowed", srcPost: "assets/gato-telefone.svg", altPost: "gato-telefone", srcLike: "assets/respondeai.svg", altLike: "respondeai", like: 1 }
+  ]
+
   return (
     <>
-      <div className="post">
-        <div className="topo">
-          <Usuario>
-            <img src="assets/img/meowed.svg" alt="meowed" />
-            meowed
-          </Usuario>
-          <div className="acoes">
-            <ion-icon name="ellipsis-horizontal"></ion-icon>
-          </div>
-        </div>
-
-        <div className="conteudo">
-          <img src="assets/img/gato-telefone.svg" alt="gato-telefone" />
-        </div>
-
-        <div className="fundo">
-          <div className="acoes">
-            <div>
-              <ion-icon name="heart-outline"></ion-icon>
-              <ion-icon name="chatbubble-outline"></ion-icon>
-              <ion-icon name="paper-plane-outline"></ion-icon>
-            </div>
-            <div>
-              <ion-icon name="bookmark-outline"></ion-icon>
+      {objPost.map(i => 
+          <div className="post">
+          <div className="topo">
+            <Usuario>
+              <img src={i.srcTopo} alt={i.altTopo} />
+              {i.altTopo}
+            </Usuario>
+            <div className="acoes">
+              <ion-icon name="ellipsis-horizontal"></ion-icon>
             </div>
           </div>
-
-          <div className="curtidas">
-            <img src="assets/img/respondeai.svg" alt="respondeai" />
-            <div className="texto">
-              Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-            </div>
+  
+          <div className="conteudo">
+            <img src={i.srcPost} alt={i.altPost} />
           </div>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="topo">
-          <Usuario>
-            <img src="assets/img/barked.svg" alt="barked" />
-            barked
-          </Usuario>
-          <div className="acoes">
-            <ion-icon name="ellipsis-horizontal"></ion-icon>
-          </div>
-        </div>
-
-        <div className="conteudo">
-          <img src="assets/img/dog.svg" alt="dog" />
-        </div>
-
-        <div className="fundo">
-          <div className="acoes">
-            <div>
-              <ion-icon name="heart-outline"></ion-icon>
-              <ion-icon name="chatbubble-outline"></ion-icon>
-              <ion-icon name="paper-plane-outline"></ion-icon>
+  
+          <div className="fundo">
+            <div className="acoes">
+              <div>
+                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon name="chatbubble-outline"></ion-icon>
+                <ion-icon name="paper-plane-outline"></ion-icon>
+              </div>
+              <div>
+                <ion-icon name="bookmark-outline"></ion-icon>
+              </div>
             </div>
-            <div>
-              <ion-icon name="bookmark-outline"></ion-icon>
-            </div>
-          </div>
-
-          <div className="curtidas">
-            <img src="assets/img/adorable_animals.svg" alt="adorable_animals" />
-            <div className="texto">
-              Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
+  
+            <div className="curtidas">
+              <img src={i.srcLike} alt={i.altLike} />
+              <div className="texto">
+                Curtido por <strong>{i.altLike}</strong> e <strong>outras {i.like} {i.like === 1 ? "pessoa" : "pessoas"}</strong>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        )}
+      
     </>
   )
 }
