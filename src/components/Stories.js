d910b153e42109f55419bd674c10bb78b83d8777
curtyraissa <raissacurty@gmail.com>
2023-02-12 20:09:33 -0300

    implementado props entre componentes Story e Stories

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,13 +1,27 @@
 import { Story } from "./Story"
 
 export const Stories = () => {
-  return (
-    <div className="stories">
-      <Story />
+  const objStory = [
+    { src: "assets/9gag.svg", alt: "9gag", nome: "9gag" },
+    { src: "assets/meowed.svg", alt: "meowed", nome: "meowed" },
+    { src: "assets/barked.svg", alt: "barked", nome: "barked" },
+    { src: "assets/nathanwpylestrangeplanet.svg", alt: "nathanwpylestrangeplanet", nome: "nathanwpylestrangeplanet"},
+    { src: "assets/wawawicomics.svg", alt: "wawawicomics", nome: "wawawicomics" },
+    { src: "assets/respondeai.svg", alt: "respondeai", nome: "respondeai" },
+    { src: "assets/filomoderna.svg", alt: "filomoderna", nome: "filomoderna" },
+    { src: "assets/memeriagourmet.svg", alt: "memeriagourmet", nome: "memeriagourmet" }
+  ]
 
-      <div className="setinha">
-        <ion-icon name="chevron-forward-circle"></ion-icon>
-      </div>
-    </div>
+  return (
+    <>
+      {objStory.map(i =>
+        <div className="stories">
+          <Story key={i.alt} src={i.src} alt={i.alt} nome={i.nome}/>
+          <div className="setinha">
+            <ion-icon name="chevron-forward-circle"></ion-icon>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
--- src/components/Story.js
+++ src/components/Story.js
@@ -1,29 +1,17 @@
 import { Usuario } from "./Usuario"
 
-export const Story = () => {
-  const objStory = [
-    { src: "assets/9gag.svg", alt: "9gag" },
-    { src: "assets/meowed.svg", alt: "meowed" },
-    { src: "assets/barked.svg", alt: "barked" },
-    { src: "assets/nathanwpylestrangeplanet.svg", alt: "nathanwpylestrangeplanet" },
-    { src: "assets/wawawicomics.svg", alt: "wawawicomics" },
-    { src: "assets/respondeai.svg", alt: "respondeai" },
-    { src: "assets/filomoderna.svg", alt: "filomoderna" },
-    { src: "assets/memeriagourmet.svg", alt: "memeriagourmet" }
-  ]
+export const Story = (props) => {
 
   return (
     <>
-      {objStory.map(i =>
         <div className="story">
           <div className="imagem">
-            <img src={i.src} alt={i.alt} />
+            <img src={props.src} alt={props.alt} />
           </div>
           <Usuario>
-            {i.alt}
+            {props.nome}
           </Usuario>
         </div>
-      )}
 
     </>
   )
