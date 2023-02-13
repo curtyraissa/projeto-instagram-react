b23e0c04684b38b264b4e06f6ab3db90b2d6d200
curtyraissa <raissacurty@gmail.com>
2023-02-13 00:41:28 -0300

    consumindo props no componente filho e funtion, data-test

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,52 +1,47 @@
+import { useState } from "react"
 import { Usuario } from "./Usuario"
 
-export const Post = () => {
-  const objPost = [
-    { srcTopo: "assets/meowed.svg", altTopo: "meowed", srcPost: "assets/gato-telefone.svg", altPost: "gato-telefone", srcLike: "assets/respondeai.svg", altLike: "respondeai", like: 101.523 },
-    { srcTopo: "assets/barked.svg", altTopo: "barked", srcPost: "assets/dog.svg", altPost: "dog", srcLike: "assets/adorable_animals.svg", altLike: "adorable_animals", like: 99.159 },
-    { srcTopo: "assets/meowed.svg", altTopo: "meowed", srcPost: "assets/gato-telefone.svg", altPost: "gato-telefone", srcLike: "assets/respondeai.svg", altLike: "respondeai", like: 1 }
-  ]
+export const Post = (props) => {
+  const [heart, setHeart] = useState(true)
+  const [bookmark, setBookmark] = useState(true)
 
   return (
     <>
-      {objPost.map(i =>
-        <div className="post">
-          <div className="topo">
-            <Usuario>
-              <img src={i.srcTopo} alt={i.altTopo} />
-              {i.altTopo}
-            </Usuario>
-            <div className="acoes">
-              <ion-icon name="ellipsis-horizontal"></ion-icon>
+      <div className="post">
+        <div className="topo">
+          <Usuario>
+            <img src={props.srcTopo} alt={props.altTopo} />
+            {props.altTopo}
+          </Usuario>
+          <div className="acoes">
+            <ion-icon name="ellipsis-horizontal"></ion-icon>
+          </div>
+        </div>
+
+        <div className="conteudo">
+          <img onClick={() => {setHeart(!heart)}} src={props.srcPost} alt={props.altPost} data-test="post-image"/>
+        </div>
+
+        <div className="fundo">
+          <div className="acoes">
+            <div>
+              <ion-icon onClick={() => {setHeart(!heart)}} class={(heart === true) ? "black" : "red"} name={(heart === true) ? "heart-outline" : "heart"} data-test="like-post"></ion-icon>
+              <ion-icon name="chatbubble-outline"></ion-icon>
+              <ion-icon name="paper-plane-outline"></ion-icon>
+            </div>
+            <div>
+              <ion-icon onClick={() => {setBookmark(!bookmark) }} name={(bookmark === true) ? "bookmark-outline" : "bookmark"} data-test="save-post"></ion-icon>
             </div>
           </div>
 
-          <div className="conteudo">
-            <img src={i.srcPost} alt={i.altPost} />
-          </div>
-
-          <div className="fundo">
-            <div className="acoes">
-              <div>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </div>
-              <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-              </div>
-            </div>
-
-            <div className="curtidas">
-              <img src={i.srcLike} alt={i.altLike} />
-              <div className="texto">
-                Curtido por <strong>{i.altLike}</strong> e <strong>outras {i.like} {i.like === 1 ? "pessoa" : "pessoas"}</strong>
-              </div>
+          <div className="curtidas">
+            <img src={props.srcLike} alt={props.altLike} />
+            <div className="texto" data-test="likes-number">
+              Curtido por <strong>{props.altLike}</strong> e <strong>outras {(heart === true) ? props.like : (props.like + 0.001)} {props.like === 1 ? "pessoa" : "pessoas"}</strong>
             </div>
           </div>
         </div>
-      )}
-
+      </div>
     </>
   )
 }
