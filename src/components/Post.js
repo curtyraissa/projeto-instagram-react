c885c8ebea7f01ea11073d724b5354ae978e2f4a
curtyraissa <raissacurty@gmail.com>
2023-02-13 01:00:56 -0300

    refatorado funcao de like e salvar

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,8 +2,38 @@ import { useState } from "react"
 import { Usuario } from "./Usuario"
 
 export const Post = (props) => {
-  const [heart, setHeart] = useState(true)
-  const [bookmark, setBookmark] = useState(true)
+  const [salvar, setSalvar] = useState("bookmark-outline")
+  const [like, setLike] = useState("heart-outline")
+  const [cor, setCor] = useState("heart-outline")
+  const [curtir, setCurtir] = useState(props.number)
+
+  function salvarPost() {
+    if (salvar === "bookmark-outline") {
+      setSalvar("bookmark");
+    } else {
+      setSalvar("bookmark-outline");
+    }
+  }
+
+  function likePost() {
+    if (like === "heart-outline") {
+      setLike("heart");
+      setCor("red")
+      setCurtir(parseFloat(curtir) + 0.001)
+    } else {
+      setLike("heart-outline");
+      setCor("black")
+      setCurtir(parseFloat(curtir) - 0.001)
+    }
+  }
+
+  function likeImage() {
+    if (like === "heart-outline") {
+      setLike("heart");
+      setCor("red")
+      setCurtir(parseFloat(curtir) + 0.001)
+    }
+  }
 
   return (
     <>
@@ -19,25 +49,25 @@ export const Post = (props) => {
         </div>
 
         <div className="conteudo">
-          <img onClick={() => {setHeart(!heart)}} src={props.srcPost} alt={props.altPost} data-test="post-image"/>
+          <img onClick={likeImage} src={props.srcPost} alt={props.altPost} data-test="post-image" />
         </div>
 
         <div className="fundo">
           <div className="acoes">
             <div>
-              <ion-icon onClick={() => {setHeart(!heart)}} class={(heart === true) ? "black" : "red"} name={(heart === true) ? "heart-outline" : "heart"} data-test="like-post"></ion-icon>
+              <ion-icon onClick={likePost} class={cor} name={like} data-test="like-post"></ion-icon>
               <ion-icon name="chatbubble-outline"></ion-icon>
               <ion-icon name="paper-plane-outline"></ion-icon>
             </div>
             <div>
-              <ion-icon onClick={() => {setBookmark(!bookmark) }} name={(bookmark === true) ? "bookmark-outline" : "bookmark"} data-test="save-post"></ion-icon>
+              <ion-icon onClick={salvarPost} name={salvar} data-test="save-post"></ion-icon>
             </div>
           </div>
 
           <div className="curtidas">
             <img src={props.srcLike} alt={props.altLike} />
             <div className="texto" data-test="likes-number">
-              Curtido por <strong>{props.altLike}</strong> e <strong>outras {(heart === true) ? props.like : (props.like + 0.001)} {props.like === 1 ? "pessoa" : "pessoas"}</strong>
+              Curtido por <strong>{props.altLike}</strong> e <strong>outras {curtir} {props.like === 1 ? "pessoa" : "pessoas"}</strong>
             </div>
           </div>
         </div>
